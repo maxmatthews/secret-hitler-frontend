67d99bc41d36e4f7a5cf82c74bcf15d8d7052a45
David Badillo <david@centrofirst.com>
2018-10-07 10:50:41 -0400

    Uploading images for later use

--- src/WaitingRoom.jsx
+++ src/WaitingRoom.jsx
@@ -12,24 +12,34 @@ export default observer(
 		render() {
 			return (
 				<div>
-					<h1>Waiting Room</h1>
-					<ul>
-						{globalStore.players.map(player => {
-							return <li key={player.socketID}>{player.name}</li>;
-						})}
-					</ul>
-					{globalStore.players.length >= 5 ? (
-						<button
-							className="btn btn-primary"
-							onClick={() => {
-								this.props.history.push("/setOrder");
-							}}
-						>
-							Start Game
-						</button>
-					) : (
-						<p>Waiting for more players</p>
-					)}
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							background: "#F7E3C1",
+							textTransform: "uppercase"
+						}}
+					>
+						<h1>Waiting Room</h1>
+						<ul>
+							{globalStore.players.map(player => {
+								return <li key={player.socketID}>{player.name}</li>;
+							})}
+						</ul>
+						{globalStore.players.length >= 5 ? (
+							<button
+								className="btn btn-primary"
+								onClick={() => {
+									this.props.history.push("/setOrder");
+								}}
+							>
+								Start Game
+							</button>
+						) : (
+							<p>Waiting for more players</p>
+						)}
+					</div>
 				</div>
 			);
 		}
