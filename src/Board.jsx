a2692ae9f1d3f90e17f9b8e83040478bf1e2c259
Max Matthews <max@mzmtech.com>
2018-10-06 22:44:59 -0400

    Fix some idiots spacing

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,86 +2,86 @@ import React, { Component } from "react";
 import autoBind from "react-autobind";
 
 export default class Board extends Component {
-  constructor(props) {
-    super(props);
-    autoBind(this);
-  }
+	constructor(props) {
+		super(props);
+		autoBind(this);
+	}
 
-  render() {
-    return (
-      <div>
-        <div
-          style={{
-            display: "grid",
-            background: "blue",
-            gridTemplateColumns: " repeat(5, 10%) ",
-            justifyContent: "center",
-            columnGap: "2%"
-          }}
-        >
-          <img style={{ width: "100%" }} src="/policyLib.png" />
-          <img style={{ width: "100%" }} src="/policyLib.png" />
-          <img style={{ width: "100%" }} src="/policyLib.png" />
-          <img style={{ width: "100%" }} src="/policyLib.png" />
-          <img style={{ width: "100%" }} src="/policyLib.png" />
-        </div>
-        <div
-          style={{
-            display: "grid",
-            background: "red",
-            gridTemplateColumns: " repeat(6, 10%) ",
-            justifyContent: "center",
-            columnGap: "2%"
-          }}
-        >
-          <img style={{ width: "100%" }} src="/policyFas.png" />
-          <img style={{ width: "100%" }} src="/policyFas.png" />
-          {/* <img style={{ width: "100%" }} src="/policyFas.png" />
+	render() {
+		return (
+			<div>
+				<div
+					style={{
+						display: "grid",
+						background: "blue",
+						gridTemplateColumns: " repeat(5, 10%) ",
+						justifyContent: "center",
+						columnGap: "2%"
+					}}
+				>
+					<img style={{ width: "100%" }} src="/policyLib.png" />
+					<img style={{ width: "100%" }} src="/policyLib.png" />
+					<img style={{ width: "100%" }} src="/policyLib.png" />
+					<img style={{ width: "100%" }} src="/policyLib.png" />
+					<img style={{ width: "100%" }} src="/policyLib.png" />
+				</div>
+				<div
+					style={{
+						display: "grid",
+						background: "red",
+						gridTemplateColumns: " repeat(6, 10%) ",
+						justifyContent: "center",
+						columnGap: "2%"
+					}}
+				>
+					<img style={{ width: "100%" }} src="/policyFas.png" />
+					<img style={{ width: "100%" }} src="/policyFas.png" />
+					{/* <img style={{ width: "100%" }} src="/policyFas.png" />
           <img style={{ width: "100%" }} src="/policyFas.png" />
           <img style={{ width: "100%" }} src="/policyFas.png" />
           <img style={{ width: "100%" }} src="/policyFas.png" /> */}
-          <p
-            style={{
-              color: "#fff",
-              gridColumn: "3/4",
-              gridRow: "1/2",
-              fontSize: "12px"
-            }}
-          >
-            The President Examines The Top Three Cards
-          </p>
-          <p
-            style={{
-              color: "#fff",
-              gridColumn: "4/5",
-              gridRow: "1/2",
-              fontSize: "12px"
-            }}
-          >
-            The President Must Kill A Player
-          </p>
-          <p
-            style={{
-              color: "#fff",
-              gridColumn: "5/6",
-              gridRow: "1/2",
-              fontSize: "12px"
-            }}
-          >
-            The President Must Kill A Player.Veto Power is enabled
-          </p>
-          <p
-            style={{
-              color: "#fff",
-              gridColumn: "6/7",
-              gridRow: "1/2",
-              fontSize: "12px"
-            }}
-          >
-            Skull Face
-          </p>
-        </div>
-      </div>
-    );
-  }
+					<p
+						style={{
+							color: "#fff",
+							gridColumn: "3/4",
+							gridRow: "1/2",
+							fontSize: "12px"
+						}}
+					>
+						The President Examines The Top Three Cards
+					</p>
+					<p
+						style={{
+							color: "#fff",
+							gridColumn: "4/5",
+							gridRow: "1/2",
+							fontSize: "12px"
+						}}
+					>
+						The President Must Kill A Player
+					</p>
+					<p
+						style={{
+							color: "#fff",
+							gridColumn: "5/6",
+							gridRow: "1/2",
+							fontSize: "12px"
+						}}
+					>
+						The President Must Kill A Player.Veto Power is enabled
+					</p>
+					<p
+						style={{
+							color: "#fff",
+							gridColumn: "6/7",
+							gridRow: "1/2",
+							fontSize: "12px"
+						}}
+					>
+						Skull Face
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
